20eb8dc6d7c3af58444e71d5ea8ed5e476967f12
Aleksandar Gulevski <agulevski10@gmail.com>
2019-02-26 21:37:57 +0100

    First commit

---
+++ app/src/main/java/com/aleksandar/gulevski/switchbetweenactivitiesexercise/MainActivity.java
@@ -0,0 +1,31 @@
+package com.aleksandar.gulevski.switchbetweenactivitiesexercise;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onClickNextButton();
+    }
+
+    public void onClickNextButton (){
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              startActivity(new Intent(MainActivity.this, SecondActivity.class));
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/aleksandar/gulevski/switchbetweenactivitiesexercise/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.aleksandar.gulevski.switchbetweenactivitiesexercise;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        onClickBackButton();
+    }
+
+    public void onClickBackButton () {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              finish();
+
+            }
+    });
+}
+}
